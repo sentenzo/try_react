6f20a8c28e42f823703a6fc9a27dbc71bde39c59
sentenzo <kassiyskley@rambler.ru>
2022-10-26 15:29:19 +0300

    notes-app preset

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from "react"
+import ReactDOM from "react-dom"
 
+import App from "./App"
 
-import "./index.css"
-
-
-const App = () => <div>
-
-</div>
-
-const root = ReactDOM.createRoot(
-    document.getElementById("root")
-);
-root.render(<App />);
-// root.render(<h1>Hi</h1>);
+ReactDOM.render(<App />, document.getElementById("root"))
