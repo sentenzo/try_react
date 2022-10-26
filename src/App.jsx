f2fce8dc720b3d578274707469d8cf998a2d9417
sentenzo <kassiyskley@rambler.ru>
2022-10-26 15:15:11 +0300

    implementing dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
+import React from "react"
+
 import Navbar from "./components/Navbar"
 import Main from "./components/Main"
 
 import css from "./index.css"
 
-const App = () =>
-    <div className="container">
-        <Navbar />
-        <Main />
-    </div>
+const App = () => {
+    const [darkMode, set_darkMode] = React.useState(false);
+    const toggle_darkMode = () => set_darkMode(old_val => !old_val)
+
+    return <div className="container">
+        <Navbar darkMode={darkMode} toggleDarkMode={toggle_darkMode} />
+        <Main darkMode={darkMode} />
+    </div>;
+}
 
 export default App;
