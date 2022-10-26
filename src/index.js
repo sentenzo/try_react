c952fd099a9e06d307b794ad0da549fe06b4ba25
sentenzo <kassiyskley@rambler.ru>
2022-10-26 15:18:24 +0300

    cleaning up

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import App from "./App"
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById("root"))
+
+import "./index.css"
+
+
+const App = () => <div>
+
+</div>
+
+const root = ReactDOM.createRoot(
+    document.getElementById("root")
+);
+root.render(<App />);
+// root.render(<h1>Hi</h1>);
